d401fa7f400ffed91cc851d7204f0f5a76609d89
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2023-03-04 17:56:20 +0800

    适配v16

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import odoo
 
-def post_init_hook(cr, reg):
-    env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+def post_init_hook(env):
+    # env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
     odoo_root = env.ref("base.partner_root")
-    odoo_root.write({"name": "PPMessageBot", "email": "ppmessagebot@example.com"})
-    return
+    odoo_root.write({"name": "MocoBot", "email": "mocobot@example.com"})
+    
